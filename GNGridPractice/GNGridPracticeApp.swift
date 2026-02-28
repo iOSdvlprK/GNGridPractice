1eb63e8a2677fc38f0af966139e1e55fa965ebb0
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-02-28 23:09:20 +0900

    Initial Commit

---
+++ GNGridPractice/GNGridPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNGridPracticeApp.swift
+//  GNGridPractice
+//
+//  Created by joe on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct GNGridPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
