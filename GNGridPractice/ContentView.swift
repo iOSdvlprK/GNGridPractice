71ed3472de2b0adf32353461286bd09cfa4e1430
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-03-01 23:16:20 +0900

    exercise Grid UI

--- GNGridPractice/ContentView.swift
+++ GNGridPractice/ContentView.swift
@@ -10,12 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Grid {
+                GridRow {
+                    Text("Exercise Name")
+                        .font(.system(size: 15, weight: .semibold))
+                        .frame(maxWidth: .infinity)
+                    Text("Sets")
+                        .font(.system(size: 15, weight: .semibold))
+                    Text("Reps")
+                        .font(.system(size: 15, weight: .semibold))
+                    Text("Weight (kg)")
+                        .font(.system(size: 15, weight: .semibold))
+                }
+                Divider()
+                GridRow {
+                    Text("Bench Press")
+                        .font(.system(size: 15))
+                        .frame(maxWidth: .infinity)
+                    Text("3")
+                        .font(.system(size: 15))
+                    Text("6")
+                        .font(.system(size: 15))
+                    Text("80")
+                        .font(.system(size: 15))
+                }
+            }
+            .padding(.horizontal)
         }
-        .padding()
     }
 }
 
